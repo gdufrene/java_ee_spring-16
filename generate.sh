c47359efb6dbd469326a774a4a3f7b2fa29450a3
Guillaume <gdufrene@keyneosoft.fr>
2016-09-18 15:54:26 +0200

    Rework generate, restructure files

--- generate.sh
+++ generate.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
 
 function gen() {
-  cat tpl/header.html tpl/$1.html tpl/footer.html > $1.html
+  files=""
+  outfile="$1.html"
+  test $# -gt 1 && shift
+  for file in $@; do
+    files="$files tpl/$file.html"
+  done
+  cat tpl/header.html $files tpl/footer.html > $outfile
 }
 
 gen index
+
+gen java
+gen maven
+gen tomcat
+gen devops_tools
+gen git
+
+gen servlet
+
+gen week1
+
+gen sitemap
